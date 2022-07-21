884e6b79c399d19692fffa0db67f134fdb5dc0b9
tnp3011 <phu.tn1309@gmail.com>
2022-07-21 09:03:07 +0700

    add soft delete

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const slug = require("mongoose-slug-generator");
-
-mongoose.plugin(slug);
+const mongooseDelete = require("mongoose-delete");
 
 const Schema = mongoose.Schema;
 
@@ -16,5 +15,12 @@ const Course = new Schema(
     },
     { timestamps: true },
 );
+
+// Add plugin methods
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    // deleteAt: true,
+    overrideMethods: "all",
+});
 
 module.exports = mongoose.model("Course", Course);
